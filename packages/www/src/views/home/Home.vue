6a01c7817970ab6e1f836cb3ad35a6a555c1b446
gabrielmoraisdev4 <gabrielmoraisdev4@gmail.com>
2025-11-08 17:03:17 +0100

    add: leaflet map setup

--- packages/www/src/views/home/Home.vue
+++ packages/www/src/views/home/Home.vue
@@ -1,8 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const mapContainer = ref<HTMLElement | null>(null)
+let map: L.Map | null = null
+
+onMounted(() => {
+  if (!mapContainer.value) return
+
+  map = L.map(mapContainer.value).setView([48.8566, 2.3522], 13) // Paris como exemplo
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
+  }).addTo(map)
+
+  const marker = L.marker([48.8566, 2.3522]).addTo(map)
+  marker.bindPopup('<b>Paris</b><br>Location').openPopup()
+})
+
+onUnmounted(() => {
+  if (map) {
+    map.remove()
+    map = null
+  }
+})
+</script>
 
 <template>
-    <div>
-        <h1>Bienvenue à la page d'accueil</h1>
-        <p>Ceci est le contenu de la page d'accueil.</p>
-    </div>
+  <div class="map-wrapper">
+    <div ref="mapContainer" class="map-container"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.map-wrapper {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+
+.map-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
